c70b17926b03ac70a3f0167f3d902b0cede4347c
Dürauer Markus <markus.duerauer@feuerwehr.gv.at>
2017-10-21 15:14:30 +0200

    Beispiel 2

--- src/main/java/at/ac/is161505/hashtag/MapUtils.java
+++ src/main/java/at/ac/is161505/hashtag/MapUtils.java
@@ -9,12 +9,9 @@ public class MapUtils {
 
     public static Map<String, Integer> sortByValueDesc(Map<String, Integer> unsortMap) {
 
-        // 1. Convert Map to List of Map
         List<Map.Entry<String, Integer>> list =
                 new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
 
-        // 2. Sort list with Collections.sort(), provide a custom Comparator
-        //    Try switch the o1 o2 position for a different order
         Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1,
                                Map.Entry<String, Integer> o2) {
@@ -22,7 +19,6 @@ public class MapUtils {
             }
         });
 
-        // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
         Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
         for (Map.Entry<String, Integer> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
